078770dd31aab4023bd6570a4578fd9769ce611a
Nicholas Di Girolamo <nicholasdiggi@gmail.com>
2024-04-06 16:46:08 -0500

    put courses into bootstrap cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 const schedule = {
   title: "CS Courses for 2018-2019",
@@ -27,7 +28,7 @@ const schedule = {
 };
 
 const App = () => (
-  <div>
+  <div className='container'>
     <Banner title={ schedule.title} />
     <CourseList courses={ schedule.courses } />
   </div>
@@ -40,7 +41,7 @@ const Banner = ({ title }) => (
 
 // CourseList component
 const CourseList = ({ courses }) => (
-  <div>
+  <div className='course-list'>
     { Object.values(courses).map(course => <Course key={course.id} course={course} />) }
   </div>
 );
@@ -58,8 +59,11 @@ const getCourseNumber = course => (
 
 // Course component
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <div className='card m-1 p-2'>
+    <div className='card-body'>
+      <div className='card-title'>{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className='card-text'>{ course.title }</div>
+    </div>
   </div>
 );
 
