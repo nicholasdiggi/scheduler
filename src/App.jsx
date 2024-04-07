c30fbea0559b7a52fb050422ad24b62c66636ae9
Nicholas Di Girolamo <nicholasdiggi@gmail.com>
2024-04-06 19:16:25 -0500

    added buttons to filter by quarter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
@@ -67,19 +67,36 @@ const Banner = ({ title }) => (
 );
 
 // CourseList component
-const CourseList = ({ courses }) => (
-  <div className='course-list'>
-    { Object.values(courses).map(course => <Course key={course.id} course={course} />) }
-  </div>
-);
+const CourseList = ({ courses }) => {
+  // Find the courses that match the selected term
+  const [term, setTerm] = useState('Fall');
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className='course-list'>
+        {/* Display all courses */}
+        {/* { Object.values(courses).map(course => <Course key={course.id} course={course} />) } */}
+
+        {/* Only display the courses that match the selected term */}
+        { termCourses.map(course => <Course key={course.id} course={course} />) }
+      </div>
+    </>
+  );
+};
 
 // Course component helper functions
-// const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
 
+//// Original getCourseTerm function (before using url data)
 // const getCourseTerm = course => (
 //   terms[course.id.charAt(0)]
 // );
 
+const getCourseTerm = course => course.term;
+
+//// Original getCourseNumber function (before using url data)
 // const getCourseNumber = course => (
 //   course.id.slice(1, 4)
 // );
@@ -93,6 +110,27 @@ const Course = ({ course }) => (
       <div className='card-subtext'>{ course.meets }</div>
     </div>
   </div>
+);
+
+// TermSelector component
+const TermSelector = ({term, setTerm}) => (
+  <div className='btn-group'>
+    {
+      Object.values(terms).map(value =>
+        <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />)
+    }
+  </div>
+);
+
+// TermButton component
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type='radio' id={term} className='btn-check' checked={checked} autoComplete='off'
+      onChange={ () => setTerm(term) } />
+    <label class='btn btn-success m-1 p-2' htmlFor={term}>
+      {term}
+    </label>
+  </>
 );
 
 export default App;
