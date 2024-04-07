26033f2fe68b6bcf8008f98759011cb5ba82847c
ysilksa <142238359+ysilksa@users.noreply.github.com>
2024-04-06 19:20:17 -0500

    Update priorityqueue.js, correction to comparator

--- priorityqueue/priorityqueue.js
+++ priorityqueue/priorityqueue.js
@@ -3,7 +3,7 @@ const AssignmentClass = require('./assignment.js');
 var assert = require('assert');
 
 var comparePriority = function(assign1, assign2) {return assign1.priority - assign2.priority}
-var assignqueue = new PriorityQueue(comparePriority);
+var assignqueue = new PriorityQueue({comparator: comparePriority});
 
 const assign1 = new AssignmentClass.Assignment("Essay 1", 40, 20, 3);
 const assign2 = new AssignmentClass.Assignment("HW 2", 100, 60, 1);
@@ -11,10 +11,13 @@ const assign2 = new AssignmentClass.Assignment("HW 2", 100, 60, 1);
 // obv assign1 > assign2 in priority
 //console.log(assign1.deadline);
 
-
 // basic test case
-assignqueue.queue(assign1);
-assignqueue.queue(assign2);
-var first = assignqueue.dequeue();
+// assignqueue.queue(assign1);
+// assignqueue.queue(assign2);
+// var first = assignqueue.dequeue();
+// var second = assignqueue.dequeue();
 
-assert(first == assign1);
+// assert(first == assign1);
+
+
+
